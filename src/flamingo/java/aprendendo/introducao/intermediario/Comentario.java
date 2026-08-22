fc7b8827d2f82ed45bf975155f4bcca7e5c5bf6c
yurikoga <yurikogasantos565@gmail.com>
2026-08-21 22:46:29 -0300

    atividades do professor bigode

---
+++ src/flamingo/java/aprendendo/introducao/intermediario/Comentario.java
@@ -0,0 +1,17 @@
+package flamingo.java.aprendendo.introducao.intermediario;
+/** Apresentação de Comentários*/
+
+// O /** serve para criar uma documentação*/
+public class Comentario {
+	static void main() {
+		//Comentário de linha
+		/*Comentário
+		* de
+		* várias
+		* linhas
+		* */
+
+		//teste
+		System.out.println("Comentário");
+	}
+}
---
+++ src/flamingo/java/aprendendo/introducao/intermediario/OlaMundo.java
@@ -0,0 +1,9 @@
+package flamingo.java.aprendendo.introducao.intermediario;
+
+public class OlaMundo {
+	public static void main(String[] args){
+		System.out.println("Olá, Mundo!");
+
+		//teste
+	}
+}
